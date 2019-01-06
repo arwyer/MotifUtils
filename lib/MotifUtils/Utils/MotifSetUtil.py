2c5303b0862c0b760eb735726ba4e3c4506cc3d6
arwyer <awyer@vols.utk.edu>
2019-01-06 16:44:01 -0500

    background/sequence

--- lib/MotifUtils/Utils/MotifSetUtil.py
+++ lib/MotifUtils/Utils/MotifSetUtil.py
@@ -1,5 +1,25 @@
 from copy import deepcopy
 
+def GetBackground():
+    seqfile = '/kb/module/work/tmp/SeqSet.fa'
+    count = 0
+    sfile = open(seqfile)
+    FreqDict = {'A':0,'G':0,'C':0,'T':0}
+    for line in sfile:
+        if count%2 == 1:
+            FreqDict['A'] += line.count('A')
+            FreqDict['C'] += line.count('C')
+            FreqDict['G'] += line.count('G')
+            FreqDict['T'] += line.count('T')
+        count += 1
+    total = FreqDict['A'] + FreqDict['C'] + FreqDict['G'] + FreqDict['T']
+    Background = {}
+    Background['A'] = float(FreqDict['A'])/total
+    Background['C'] = float(FreqDict['C'])/total
+    Background['G'] = float(FreqDict['G'])/total
+    Background['T'] = float(FreqDict['T'])/total
+    return Background
+
 def CheckLength(motifset,min,max):
     topop = []
     for i,motif in enumerate(motifset['Motifs']):
@@ -11,9 +31,36 @@ def CheckLength(motifset,min,max):
 
 
 #TODO: extract sequence from output files, add to motif object
+def BuildSetDict():
+    seqfile = '/kb/module/work/tmp/SeqSet.fa'
+    count = 0
+    sfile = open(seqfile)
+    seqDict = {}
+    id = ''
+    for line in sfile:
+        if count%2 == 0:
+            id = line.replace('\n','').replace('>','')
+        else:
+            seqDict[id] = line.replace('\n','')
+        count += 1
+
+def ExtractSequence(start,end,orientation,id,SeqDict):
+    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A','N':'N'}
+    if orientation = '+':
+        return SeqDict[id][start:end]
+    else:
+        tempseq = SeqDict[id][start:end]
+        newSeq = ''
+        for b in tempseq:
+            newSeq += complement[b]
+        newSeq = [::-1]
+        return newSeq
+
+
 def ConvertMotif(motif,MotifSet):
     newMotif = {}
     newMotif['Motif_Locations'] = []
+    SeqDict = BuildSetDict()
     for loc in motif['Locations']:
         new_loc = {}
         #new_loc['Feature_id'] = loc[0]
@@ -21,7 +68,7 @@ def ConvertMotif(motif,MotifSet):
         new_loc['start'] = int(loc[1])
         new_loc['end'] = int(loc[2])
         new_loc['orientation'] = loc[3]
-        new_loc['sequence']= 'SEQUENCE'
+        new_loc['sequence']= ExtractSequence(int(loc[1]),int(loc[2]),loc[3],loc[0],SeqDict)
         newMotif['Motif_Locations'].append(new_loc.copy())
     newMotif['Iupac_sequence'] = motif['Iupac_signature']
     newMotif['PWM'] = {}
