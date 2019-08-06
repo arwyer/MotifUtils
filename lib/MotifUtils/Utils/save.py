4399415f34d70aa6989f1a35e242a6d6c91120d4
Ryan Routsong <rroutson@utk.edu>
2019-08-06 16:20:49 -0400

    fix saveMotifSet output errors

--- lib/MotifUtils/Utils/save.py
+++ lib/MotifUtils/Utils/save.py
@@ -18,25 +18,29 @@ class MotifSaver:
             # TODO: accept lists of constructed motif set object
             # TODO: check if list is a save_objects list or list of motifsets process accordingly
             # TODO: accept list of object names
-            self.dfu.save_objects({
+            obj = self.dfu.save_objects({
                 'id': self.dfu.ws_name_to_id(params['ws_name']),
                 'objects': [{
                     'type': 'KBaseGeneRegulation.MotifSet',
                     'data': motifset[0],
                     'name': str(uuid.uuid4())
                 }]
-            })
+            })[0]
+
+            return str(obj[6]) + "/" + str(obj[0]) + "/" + str(obj[4])
         elif isinstance(motifset, dict):
             logging.info('Saving a single motifset object...')
             # TODO: accept object name
-            self.dfu.save_objects({
+            obj = self.dfu.save_objects({
                 'id': self.dfu.ws_name_to_id(params['ws_name']),
                 'objects': [{
                     'type': 'KBaseGeneRegulation.MotifSet',
                     'data': motifset,
                     'name': str(uuid.uuid4())
                 }]
-            })
+            })[0]
+
+            return str(obj[6]) + "/" + str(obj[0]) + "/" + str(obj[4])
         else:
             raise ValueError('Input to motif saver should be either: ' + '\n'
                              '1. a list of constructed KBaseGeneRegulation.MotifSet objects (dictionary)\n' +
