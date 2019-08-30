e21591ac54bd0e0687f27f407fb4a71a6ab52e82
Ryan Routsong <rroutsong@asuragen.com>
2019-08-30 09:51:33 -0500

    fix motif utils saver

--- lib/MotifUtils/Utils/save.py
+++ lib/MotifUtils/Utils/save.py
@@ -13,35 +13,15 @@ class MotifSaver:
                             level=logging.INFO)
 
     def saveMotifSet(self, motifset, params):
-        if isinstance(motifset, list):
-            logging.info('Saving multiple motifset objects...')
-            # TODO: accept lists of constructed motif set object
-            # TODO: check if list is a save_objects list or list of motifsets process accordingly
-            # TODO: accept list of object names
-            obj = self.dfu.save_objects({
-                'id': self.dfu.ws_name_to_id(params['ws_name']),
-                'objects': [{
-                    'type': 'KBaseGeneRegulation.MotifSet',
-                    'data': motifset,
-                    'name': str(uuid.uuid4())
-                }]
-            })[0]
+        logging.info('Saving a single motifset object...')
+        # TODO: accept object name
+        obj = self.dfu.save_objects({
+            'id': self.dfu.ws_name_to_id(params['ws_name']),
+            'objects': [{
+                'type': 'KBaseGeneRegulation.MotifSet',
+                'data': [motifset],
+                'name': str(uuid.uuid4())
+            }]
+        })[0]
 
-            return str(obj[6]) + "/" + str(obj[0]) + "/" + str(obj[4])
-        elif isinstance(motifset, dict):
-            logging.info('Saving a single motifset object...')
-            # TODO: accept object name
-            obj = self.dfu.save_objects({
-                'id': self.dfu.ws_name_to_id(params['ws_name']),
-                'objects': [{
-                    'type': 'KBaseGeneRegulation.MotifSet',
-                    'data': [motifset],
-                    'name': str(uuid.uuid4())
-                }]
-            })[0]
-
-            return str(obj[6]) + "/" + str(obj[0]) + "/" + str(obj[4])
-        else:
-            raise ValueError('Input to motif saver should be either: ' + '\n'
-                             '1. a list of constructed KBaseGeneRegulation.MotifSet objects (dictionary)\n' +
-                             '2. a single KBaseGeneRegulation.MotifSet object (dictionary)')
+        return str(obj[6]) + "/" + str(obj[0]) + "/" + str(obj[4])
